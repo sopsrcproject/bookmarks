67ca658f3d2d14f85fc9cd6e6a8157dde22deac5
TanNguyen <mntan998@gmail.com>
2017-03-15 18:23:17 +0700

    Init circle test

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.contrib.staticfiles import finders
 
-# Create your tests here.
+
+class TestStaticFiles(TestCase):
+    """Check if app contains required static files"""
+    def test_css(self):
+        abs_path = finders.find('css/base.css')
+        self.assertIsNotNone(abs_path)
